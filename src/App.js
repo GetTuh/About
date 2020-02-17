f9560ed8ce3d2eee3b0931b262c89516bbd14ad1
GetTuh <ktosspl@gmail.com>
2020-02-17 17:04:35 +0100

    Removed react-anime and react-bootstrap, preetifying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AnimatedMainSwitch from './components/AnimatedMainSwitch'
-const App = ()=>(
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AnimatedMainSwitch from "./components/AnimatedMainSwitch";
+const App = () => (
   <div>
-    <AnimatedMainSwitch/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
+    <AnimatedMainSwitch />
   </div>
-)
+);
 
 export default App;
--- src/components/AnimatedMainSwitch.js
+++ src/components/AnimatedMainSwitch.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {mapStyles, bounceTransition} from '../animations/mainRouterTransition'
-import { AnimatedSwitch } from 'react-router-transition';
-import welcomeScreen from './welcomeScreen'
+import { mapStyles, bounceTransition } from "./mainRouterTransition";
+import { AnimatedSwitch } from "react-router-transition";
+import welcomeScreen from "./welcome/welcomeScreen";
 
+const B = () => <div className="display-1">B</div>;
+const C = () => <div className="">C</div>;
 
-const A = ()=>(
-  <div className="display-1"><Link to='/b'>A</Link></div>
-)
-const B = ()=>(
-  <div className="display-1">B</div>
-)
-const C = ()=>(
-  <div className="">C</div>
-)
-
-export default()=>(
-<Router>
-  <AnimatedSwitch
-    atEnter={bounceTransition.atEnter}
-    atLeave={bounceTransition.atLeave}
-    atActive={bounceTransition.atActive}
-    mapStyles={mapStyles}
-    className="route-wrapper"
-  >
-    <Route path="/About/" component={welcomeScreen} />
-    <Route path="/B" component={B} />
-    <Route path="/c" component={C} />
-  </AnimatedSwitch>
+export default () => (
+  <Router >
+    <AnimatedSwitch
+      atEnter={bounceTransition.atEnter}
+      atLeave={bounceTransition.atLeave}
+      atActive={bounceTransition.atActive}
+      mapStyles={mapStyles}
+      className="route-wrapper"
+    >
+      <Route path="/About/" component={welcomeScreen} />
+      <Route path="/B" component={B} />
+      <Route path="/c" component={C} />
+    </AnimatedSwitch>
   </Router>
-  )
+);
